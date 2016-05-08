95e875068c88ca5bd782aaf22023978f5d0c05f0
Ty Overby <ty@pre-alpha.com>
2016-05-08 12:38:54 -0700

    Add multiple scrollbar types.

--- src/components/scrollbar.rs
+++ src/components/scrollbar.rs
@@ -7,29 +7,55 @@ pub trait ScrollbarRender {
     fn draw_scrollbar(&mut self, id: &Id, entire_rect: Rect<f32>, drag_rect: Rect<f32>, over: bool, down: bool) -> Result<(), Self::Error>;
 }
 
-pub fn scrollbar<E, A>(ctx: &mut UiContext<A>, id: Id, rect: Rect<f32>, container_size: f32, contents_size: f32, scroll_pos: &mut f32) -> Result<bool, E>
+pub fn scrollbar_v<E, A>(ctx: &mut UiContext<A>, id: Id, rect: Rect<f32>, container_size: f32, contents_size: f32, scroll_pos: &mut f32) -> Result<bool, E>
 where A: ScrollbarRender<Error=E> + DragRegionRender<Error=E>
 {
-    let (w, h) = (rect.width(), rect.height());
-    let percent_covering = (container_size / contents_size).min(1.0).max(0.0);
-    let size_covering = percent_covering * h;
-    let starting_at = (*scroll_pos / contents_size) * h;
-    let height_minus_dead_zone = h - size_covering;
+    scrollbar(ctx, id, rect, container_size, contents_size, scroll_pos,
+        Rect::height,
+        Rect::top,
+        Rect::set_top,
+        |dragged, basis| dragged.set_left(basis.left()),
+        |outer, starting_at, size_covering| Rect::xywh(outer.left(), outer.top() + starting_at, outer.width(), size_covering))
+}
+pub fn scrollbar_h<E, A>(ctx: &mut UiContext<A>, id: Id, rect: Rect<f32>, container_size: f32, contents_size: f32, scroll_pos: &mut f32) -> Result<bool, E>
+where A: ScrollbarRender<Error=E> + DragRegionRender<Error=E>
+{
+    scrollbar(ctx, id, rect, container_size, contents_size, scroll_pos,
+        Rect::width,
+        Rect::left,
+        Rect::set_left,
+        |dragged, basis| dragged.set_top(basis.top()),
+        |outer, starting_at, size_covering| Rect::xywh(outer.left() + starting_at, outer.top(), size_covering, outer.height()))
+}
 
-    let Point(x, y) = rect.top_left();
-    let mut bar_rect = Rect::xywh(x, y + starting_at, w, size_covering);
+pub fn scrollbar<E, A, DG, DS, CR, CB, SZ>(ctx: &mut UiContext<A>, id: Id, rect: Rect<f32>, container_size: f32, contents_size: f32, scroll_pos: &mut f32, size_getter: SZ, direction_getter: DG, direction_setter: DS, correct: CR, construct_bar: CB) -> Result<bool, E>
+where A: ScrollbarRender<Error=E> + DragRegionRender<Error=E>,
+      DG: Fn(Rect<f32>) -> f32,
+      DS: Fn(Rect<f32>, f32) -> Rect<f32>,
+      CR: Fn(Rect<f32>, Rect<f32>) -> Rect<f32>,
+      CB: Fn(Rect<f32>, f32, f32) -> Rect<f32>,
+      SZ: Fn(Rect<f32>) -> f32,
+{
+    let sz = size_getter(rect);
+    let percent_covering = (container_size / contents_size).min(1.0).max(0.0);
+    let size_covering = percent_covering * sz;
+    let starting_at = (*scroll_pos / contents_size) * sz;
+    let height_minus_dead_zone = sz - size_covering;
+
+    let mut bar_rect = construct_bar(rect, starting_at, size_covering);
 
     ctx.with_other_backend(&mut NullRenderer, |ctx| {
         drag_region(ctx, id!().with_parent(id.clone()), &mut bar_rect).unwrap();
     });
 
-    bar_rect = bar_rect.set_left(rect.left());
-    bar_rect = bar_rect.set_top(
-        clamp(rect.top(),
-              bar_rect.top(),
-              rect.top() + height_minus_dead_zone));
+    bar_rect = correct(bar_rect, rect);
+    bar_rect =
+        direction_setter(bar_rect,
+            clamp(direction_getter(rect),
+                  direction_getter(bar_rect),
+                  direction_getter(rect) + height_minus_dead_zone));
 
-    *scroll_pos = ((bar_rect.top() - rect.top()) * contents_size) / h;
+    *scroll_pos = ((direction_getter(bar_rect) - direction_getter(rect)) * contents_size) / sz;
 
     let &mut UiContext{ref mut backend, ..} = ctx;
     let over = false;
